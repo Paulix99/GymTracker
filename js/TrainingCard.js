1757b3a455e0bb97b748c881a169026083219711
IV <ivandiscobolo@gmail.com>
2025-08-11 18:55:16 +0200

    Arreglado disposicion del atributo training

--- js/TrainingCard.js
+++ js/TrainingCard.js
@@ -4,12 +4,13 @@ class TrainingCard{
 		this.user = user;
 		this.type = "";
 		this.training = [
-			{
+			/*{
 				exercise: "",
-				sets: [],
-				reps: [],
-				weight: []
-			}
+				sets: [{
+					reps: 0,
+					weight: 0
+				}]
+			}*/
 		];
 	}
 
@@ -17,39 +18,61 @@ class TrainingCard{
 		this.type = type;
 	}
 
-	updateExercise(exercise_index, set_index, exercise, sets, reps, weight){
-		this.training[exercise_index].exercise = exercise;
-		this.training[exercise_index].sets.splice(set_index, 1, set_index + 1);
-		this.training[exercise_index].reps.splice(set_index, 1, reps);
-		this.training[exercise_index].weight.splice(set_index, 1, weight);
-	}
-
 	addNewExercise(){
 		this.training.push(
 			{
 				exercise: "",
-				sets: [],
-				reps: [],
-				weight: []
+				sets: [{
+					reps: 0,
+					weight: 0
+				}]
 			}
 		);
+	}
+
+	addNewSet(exercise_index){
+		this.training[exercise_index].sets.push(
+			{
+				reps: 0,
+				weight: 0
+			}
+		);
+	}
+
+	updateExercise(exercise_index, set_index, exercise, reps, weight){
+		if(this.training.length <= exercise_index){
+			this.addNewExercise();
+		}
+
+		if(this.training[exercise_index].sets.length <= set_index){
+			this.addNewSet(exercise_index);
+		}
+
+		this.training[exercise_index].exercise = exercise;
+		this.training[exercise_index].sets[set_index].reps = reps;
+		this.training[exercise_index].sets[set_index].weight = weight;
 	}
 
 	get trainingCardInfo(){
 		let trainingInfo = [this.date, this.user, this.type]
 		for(let exercise of this.training){
 			trainingInfo.push(exercise)
+
 		}
 
 		console.log(`Done: ${trainingInfo[0]}, \nBy: ${trainingInfo[1]}, \nCategory: ${trainingInfo[2]}`);
 
 		for(let i = 3; i < trainingInfo.length; i++){
-			let exercise = trainingInfo[i]["exercise"];
-			let sets = trainingInfo[i]["sets"];
-			let reps = trainingInfo[i]["reps"];
-			let weight = trainingInfo[i]["weight"];
+				console.log(`\n\tExercise: ${trainingInfo[i].exercise}`)
 
-			console.log(`\tExercise: ${exercise}, \n\t\tSets: ${sets}, \n\t\tReps: ${reps}, \n\t\tWeight: ${weight}`);
+			for(let j = 0; j < trainingInfo[i]["sets"].length; j++){
+				let reps = trainingInfo[i]["sets"][j].reps;
+				let weight = trainingInfo[i]["sets"][j].weight;
+
+				console.log(`\t\tReps: ${reps}, \n\t\tWeight: ${weight}\n`);
+			}
+
+			
 		}
 
 		return trainingInfo;
@@ -58,16 +81,13 @@ class TrainingCard{
 
 const card = new TrainingCard("Ivan");
 card.trainingType = "Push";
-card.updateExercise(0, 0, "Press banca", 4, 10, 60);
-
-card.addNewExercise();
-card.updateExercise(1, 0, "Elevaciones laterales", 4, 10, 10);
-
-card.addNewExercise();
-card.updateExercise(2, 0, "Aperturas", 4, 10, 80);
-card.updateExercise(2, 1, "Aperturas", 4, 10, 85);
-card.updateExercise(2, 2, "Aperturas", 4, 10, 75);
+card.updateExercise(0, 0, "Press banca", 10, 60);
+card.updateExercise(1, 0, "Elevaciones laterales", 10, 10);
+card.updateExercise(2, 0, "Aperturas", 10, 80);
+card.updateExercise(2, 1, "Aperturas", 10, 85);
 
 trainingInfo = card.trainingCardInfo
 
-console.log(trainingInfo);
+console.log(trainingInfo[3]);
+console.log(trainingInfo[4])
+console.log(trainingInfo[5])
